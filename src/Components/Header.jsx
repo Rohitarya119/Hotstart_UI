df58ac07b7cbb70ee79c85a763a7da65190d3435
rohitarya119 <raviarya201016@gmail.com>
2025-01-31 16:44:18 +0530

    mayank added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,7 @@ import HeaderItem from './HeaderItem';
 function Header() {
     const menu=[
         {
-            name:'HOME',
+            name:'Mayank',
             icon:HiHome
         },
         {
